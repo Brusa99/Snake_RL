8413a9bec6fedb7dc5131cb71913be5adfd4d345
Brusa99 <simone.brusatin@gmail.com>
2023-06-30 16:24:38 +0200

    driver to train the agent

---
+++ train_driver.py
@@ -0,0 +1,80 @@
+import matplotlib.pyplot as plt
+from IPython import display
+
+from Agent import Agent
+from AIGame import SnakeGameAI
+
+
+def plot(scores, mean_scores):
+    """
+    helper function to plot the training results
+    """
+    display.clear_output(wait=True)
+    display.display(plt.gfc())
+    plt.clf()
+    plt.title("Training...")
+    plt.xlabel("# of games")
+    plt.ylabel("score")
+    plt.plot(scores)
+    plt.plot(mean_scores)
+    plt.ylim(ymin=0)
+    plt.text(len(scores)-1, scores[-1], str(scores[-1]))
+    plt.text(len(mean_scores)-1, mean_scores[-1], str(mean_scores[-1]))
+
+def train():
+    """
+    trains the agent
+    """
+    # keep track of progress
+    plot_scores = []
+    plot_mean_scores = []
+    total_score = 0
+    record = 0
+
+    # agent/environment
+    agent = Agent()
+    environment = SnakeGameAI()
+
+    # train loop
+    while True:
+        state_old = agent.get_state(environment)
+
+        # move
+        action = agent.get_action(state_old)
+        reward, game_over, score = environment.play_step(action)
+        state_new = agent.get_state(environment)
+
+        # train
+        agent.remember(state_old, action, reward, state_new, game_over)
+        agent.train_sm(state_old, action, reward, state_new, game_over)
+
+        # if tha game is lost: train long memory, reset game, plot partial results
+        if game_over:
+            # train and reset environment
+            agent.train_lm()
+            environment.reset()
+            agent.n_games += 1
+
+            # save scores
+            plot_scores.append(score)
+            total_score += score
+            plot_mean_scores.append(total_score / agent.n_games)
+
+            if score > record:
+                record = score
+                agent.model.save("data/model.pth")
+
+            # plot
+            print("Game: {}, Score: {}, Record: {}".format(agent.n_games, score, record))
+            plot(plot_scores, plot_mean_scores)
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    train()
